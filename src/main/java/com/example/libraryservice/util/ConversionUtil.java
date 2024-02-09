2b10ffc00483bd0d72604ad7a2f33d41b353973d
Azim Shamuratov <lennon645@gmail.com>
2024-02-09 09:44:24 +0300

    feat: instruction for booting and application

--- src/main/java/com/example/libraryservice/util/ConversionUtil.java
+++ src/main/java/com/example/libraryservice/util/ConversionUtil.java
@@ -3,7 +3,7 @@ package com.example.libraryservice.util;
 public class ConversionUtil {
     public static Integer convertStringToInteger(String str) {
         try {
-            return (Integer) (int) Double.parseDouble(str);
+            return (int) Double.parseDouble(str);
         } catch (NumberFormatException e) {
             return null;
         }
